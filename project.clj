1390c4304a5cb8eec0553dbc7ac513ba91122ed1
Eugene Koontz <ekoontz@hiro-tan.org>
2020-08-16 10:37:52 +0200

    stub project.xml and test input file

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject wiktionary "0.0.1-SNAPSHOT"
+  :description "Parse wiktionary dumps"
+  :url "https://github.com/ekoontz/wiktionary"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [log4j/log4j "1.2.17"]
+                 [org.clojure/tools.logging "1.1.0"]
+                 [org.clojure/data.xml "0.0.8"]])
+
+
