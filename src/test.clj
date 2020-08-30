7e2e189a73d2c912198ccbb3734861f1bd7acf67
Eugene Koontz <ekoontz@hiro-tan.org>
2020-08-30 20:05:13 +0200

    able to parse the whole thing with my own branch of data.xml

--- src/test.clj
+++ src/test.clj
@@ -1,8 +1,8 @@
 (ns wiktionary
   (:require [clojure.data.xml :as xml :refer [parse]]))
 
-(def small (with-open [input (java.io.FileInputStream. "small-notext.xml")]
-             (parse input)))
+(def small (with-open [input (java.io.FileInputStream. "small.xml")]
+             (parse input :input-buffer-length 16384)))
 
 (def tenk (with-open [input (java.io.FileInputStream. "10000.xml")]
             (parse input)))
@@ -10,7 +10,7 @@
 (def wholething (with-open
                   [input (java.io.FileInputStream.
                           "nlwiktionary-20200701-pages-articles.xml")]
-                  (parse input)))
+                  (parse input :input-buffer-length (int (Math/pow 2 27)))))
 
 (def one-page
   (nth (->> (-> wholething :content)
@@ -22,8 +22,16 @@
   (->> (-> small :content)
        (filter #(= (type %) clojure.data.xml.node.Element))))
 
-(def all-pages
+(def a-few-pages
   (->> (-> small :content)
+       (filter #(= (type %) clojure.data.xml.node.Element))
+       (filter #(= (:tag %) :xmlns.http%3A%2F%2Fwww.mediawiki.org%2Fxml%2Fexport-0.10%2F/page))))
+
+(def a-few-titles
+  (map page-title a-few-pages))
+
+(def all-pages
+  (->> (-> wholething :content)
        (filter #(= (type %) clojure.data.xml.node.Element))
        (filter #(= (:tag %) :xmlns.http%3A%2F%2Fwww.mediawiki.org%2Fxml%2Fexport-0.10%2F/page))))
 
@@ -43,12 +51,13 @@
 (defn get-revision [page]
   )
 
-(defn wtf []
-  (with-open [input (java.io.FileInputStream. "small-notext.xml")]
-    (parse input :include-node? #{:element})))
+(def fifty-titles (take 50 (map page-title all-pages)))
 
-(defn wtf2 []
-  (with-open [input (java.io.FileInputStream. "small.xml")]
-    (parse input)))
+(defn count-up-the-pages []
+  (count all-pages))
 
+(defn print-all-the-titles []
+  (doall
+   (map println
+        (sort (map page-title all-pages)))))
 
