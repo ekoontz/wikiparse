8ecba14e6f3a091af795074f66024d5e7f21c0a3
Eugene Koontz <ekoontz@hiro-tan.org>
2020-09-01 09:49:23 +0200

    more cleanup

--- src/test.clj
+++ src/test.clj
@@ -1,33 +1,34 @@
 (ns wiktionary
   (:require [clojure.data.xml :as xml :refer [parse]]))
 
-(def wholething (with-open
-                  [input (java.io.FileInputStream.
-                          "nlwiktionary-20200701-pages-articles.xml")]
-                  (parse input :input-buffer-length (int (Math/pow 2 28)))))
-
 (def all-pages
-  (->> (-> wholething :content)
-       (filter #(= (type %) clojure.data.xml.node.Element))
-       (filter #(= (:tag %) :xmlns.http%3A%2F%2Fwww.mediawiki.org%2Fxml%2Fexport-0.10%2F/page))))
+ (with-open
+   [input (java.io.FileInputStream.
+           "nlwiktionary-20200701-pages-articles.xml")]
+   (->>
+    (->
+     (parse input
+            :input-buffer-length (int (Math/pow 2 28)))
+     :content)
+    (filter #(= (type %) clojure.data.xml.node.Element))
+    (filter #(= (:tag %) :xmlns.http%3A%2F%2Fwww.mediawiki.org%2Fxml%2Fexport-0.10%2F/page)))))
 
 (defn lookup [canonical-form]
-  (let [page
-        (->> all-pages
-             (take 100000)
-             (filter #(= (page-title %) canonical-form))
-             (map :content)
-             first)]
-    (->
-     (->> page
-          (filter #(= (type %) clojure.data.xml.node.Element))
-          (filter #(= (:tag %) :xmlns.http%3A%2F%2Fwww.mediawiki.org%2Fxml%2Fexport-0.10%2F/revision))
-          (mapcat :content)
-          (filter #(= (type %) clojure.data.xml.node.Element))
-          (filter #(= (:tag %) :xmlns.http%3A%2F%2Fwww.mediawiki.org%2Fxml%2Fexport-0.10%2F/text)))
-     first
-     :content
-     first)))
+  (->
+   (->> all-pages
+        (take 100000)
+        (filter #(= (page-title %) canonical-form))
+        (map :content)
+        first
+        (filter #(= (type %) clojure.data.xml.node.Element))
+        (filter #(= (:tag %) :xmlns.http%3A%2F%2Fwww.mediawiki.org%2Fxml%2Fexport-0.10%2F/revision))
+        (mapcat :content)
+        (filter #(= (type %) clojure.data.xml.node.Element))
+        (filter #(= (:tag %) :xmlns.http%3A%2F%2Fwww.mediawiki.org%2Fxml%2Fexport-0.10%2F/text)))
+   first
+   :content
+   first))
+
 
 
 
