4896aa55f94576bff12da1e8a91ecf7b7171272a
Eugene Koontz <ekoontz@hiro-tan.org>
2020-09-16 09:30:25 +0200

    rename XSL file; number partition output by step number, not from-to range

--- partition.sh
+++ partition.sh
@@ -2,7 +2,7 @@ partition_size=30000
 for i in $(seq 0 31); do
     from=$(echo "($i * ${partition_size}) + 1" | bc -l)
     to=$(echo "($i * ${partition_size}) + ${partition_size}" | bc -l)
-    xsltproc --param from ${from} --param to ${to} one-thousand.xsl nlwiktionary-20200701-pages-articles.xml > subwiki-$from-$to.xml
+    xsltproc --param from ${from} --param to ${to} partition.xsl nlwiktionary-20200701-pages-articles.xml > subwiki-$i.xml
 done
 
 
