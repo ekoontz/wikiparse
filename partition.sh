6fb5b8b39ab1cc3e463058ed3abd8dd19f05778e
Eugene Koontz <ekoontz@hiro-tan.org>
2020-09-26 08:44:01 +0200

    30 partitions, not 31

--- partition.sh
+++ partition.sh
@@ -1,5 +1,5 @@
 partition_size=30000
-for i in $(seq 0 31); do
+for i in $(seq 0 30); do
     from=$(echo "($i * ${partition_size}) + 1" | bc -l)
     to=$(echo "($i * ${partition_size}) + ${partition_size}" | bc -l)
     xsltproc --param from ${from} --param to ${to} partition.xsl nlwiktionary-20200701-pages-articles.xml > subwiki-$i.xml
